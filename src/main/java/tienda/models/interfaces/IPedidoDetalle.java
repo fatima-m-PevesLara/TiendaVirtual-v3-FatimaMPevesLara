893252714d3adc59001b7ac6810eed2f923eae20
FatimaPeves <11200216@unmsm.edu.pe>
2021-12-13 23:59:10 -0500

    codigo Original

---
+++ src/main/java/tienda/models/interfaces/IPedidoDetalle.java
@@ -0,0 +1,19 @@
+package tienda.models.interfaces;
+
+import tienda.models.*;
+
+public interface IPedidoDetalle {
+
+    public Double calculaPrecio();
+
+    public Pedido getPedido();
+
+    public void setPedido(Pedido order);
+
+    public Integer getCantidad();
+
+    public Double getPrecio();
+
+    public String getIdProduct();
+
+}
